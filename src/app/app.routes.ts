4de9343a7e810e668dcb937d79203d9203743b43
PabloDeAlbu <pablo.cda@gmail.com>
2020-07-10 10:58:58 -0300

    Clase 22 - Login y register templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,26 @@
+import { PagesComponent } from './pages/pages.component';
+
 import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NoPageFoundComponent } from './shared/notfound/nopagefound.component';
 
 const appRoutes: Routes = [
-    { path: 'dashboard', component: DashboardComponent },
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'progress', component: ProgressComponent },
+            { path: 'greficas1', component: Graficas1Component },
+            { path: 'dashboard', component: DashboardComponent },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: LoginComponent },
-    { path: 'progress', component: ProgressComponent },
-    { path: 'greficas1', component: Graficas1Component },
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent },
     { path: '**', component: NoPageFoundComponent },
 ];
 
