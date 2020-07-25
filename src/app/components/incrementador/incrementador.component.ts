03b7450913d24b3b3ec09c9d724e0f4730c7a6f0
PabloDeAlbu <pablo.cda@gmail.com>
2020-07-25 11:11:51 -0300

    52. Refactoring de progress e incrementador. Cambio colores de incrementadores.

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -10,23 +10,29 @@ export class IncrementadorComponent implements OnInit {
 
   @Input() progreso: number = 50;
   @Input('nombre') leyenda: string = 'Leyenda';
+  @Input() btnClass: string = 'btn-primary';
+
+  @Output() valueOut: EventEmitter<number> = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit() {
+    this.btnClass = 'btn ' + this.btnClass;
   }
 
 
-  cambiarValor(valor) {
+  changeValue(valor) {
     if (valor > 0 && this.progreso + valor > 100) {
       this.progreso = 100;
-      return;
     }
-    if (valor < 0 && this.progreso + valor < 0) {
+    else if (valor < 0 && this.progreso + valor < 0) {
       this.progreso = 0;
-      return;
+    } else {
+      this.progreso += valor;
     }
+    
+    console.log(this.progreso);
+    this.valueOut.emit(this.progreso)
 
-    this.progreso += valor;
   }
 }
