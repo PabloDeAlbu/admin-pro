db877e17a89e045adca4a7ff27b17df23aedb217
PabloDeAlbu <pablo.cda@gmail.com>
2020-07-25 12:08:17 -0300

    52. Pulir detalles de nuestro incrementador component

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class IncrementadorComponent implements OnInit {
 
 
-  @Input() progreso: number = 50;
+  @Input() value: number = 50;
   @Input('nombre') leyenda: string = 'Leyenda';
   @Input() btnClass: string = 'btn-primary';
 
@@ -21,18 +21,31 @@ export class IncrementadorComponent implements OnInit {
   }
 
 
-  changeValue(valor) {
-    if (valor > 0 && this.progreso + valor > 100) {
-      this.progreso = 100;
+  changeValue(newValue) {
+    if (newValue > 0 && this.value + newValue > 100) {
+      this.value = 100;
     }
-    else if (valor < 0 && this.progreso + valor < 0) {
-      this.progreso = 0;
+    else if (newValue < 0 && this.value + newValue < 0) {
+      this.value = 0;
     } else {
-      this.progreso += valor;
+      this.value += newValue;
     }
-    
-    console.log(this.progreso);
-    this.valueOut.emit(this.progreso)
 
+    console.log(this.value);
+    this.valueOut.emit(this.value)
+
+  }
+
+  onChange(newValue: number) {
+    if (newValue > 100) {
+      newValue = 100;
+    }
+    else if (newValue < 0) {
+      newValue = 0;
+    } 
+    
+    this.value = newValue;
+
+    this.valueOut.emit(this.value)
   }
 }
